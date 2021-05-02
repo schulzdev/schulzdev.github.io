df064f4b45a31831134b126812ddba3ef42e0fc0
SchulzDEV <thefakeapp12@gmail.com>
2021-05-02 20:41:18 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,15 @@ function changeProjects() {
 function changeWelcome() {
     document.getElementById("button-welcome").innerHTML = "Home";
 }
+
+function changeGithubIcon() {
+    document.getElementById("button-github").innerHTML = '<i class="fab fa-github"></i>'
+}
+
+function changeProjectsIcon() {
+    document.getElementById("button-projects").innerHTML = '<i class="fas fa-terminal"></i>'
+}
+
+function changeWelcomeIcon() {
+    document.getElementById("button-welcome").innerHTML = '<i class="fas fa-door-open"></i>'
+}
