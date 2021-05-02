acf05b706da660aea7d3d7fcfb0bfbc61411ecee
SchulzDEV <thefakeapp12@gmail.com>
2021-05-02 19:32:52 +0200

    Add Navbar functionality

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function changeGithub() {
+    document.getElementById("button-github").innerHTML = "GitHub";
+}
+
+function changeProjects() {
+    document.getElementById("button-projects").innerHTML = "Projects";
+}
+
+function changeWelcome() {
+    document.getElementById("button-welcome").innerHTML = "Home";
+}
