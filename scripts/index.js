78dd8ad3dbd45afe6e57d720baa08be51e614fc0
SchulzDEV <thefakeapp12@gmail.com>
2021-06-05 14:44:07 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+const textWidth = 992;
 function changeGithub() {
 	document.getElementById("button-github").innerHTML = "GitHub";
 }
@@ -10,6 +11,14 @@ function changeAboutMe() {
 	document.getElementById("button-about-me").innerHTML = "About Me";
 }
 
+function changeTechno() {
+	document.getElementById("button-techno").innerHTML = "Technologies";
+}
+
+function changeContactMe() {
+	document.getElementById("button-contact-me").innerHTML = "Contact Me";
+}
+
 function changeWelcome() {
 	document.getElementById("button-welcome").innerHTML = "Home";
 }
@@ -18,36 +27,125 @@ function changeTiktok() {
 	document.getElementById("button-tiktok").innerHTML = "Dedicated TikTok Page";
 }
 
+function changeYT() {
+	document.getElementById("button-youtube").innerHTML = "YouTube";
+}
+
 function changeDiscord() {
 	document.getElementById("button-discord").innerHTML = "My Discord";
 }
 
+function changeOld() {
+	document.getElementById("old-web").innerHTML = "Old Website";
+}
+
 function changeGithubIcon() {
-	document.getElementById("button-github").innerHTML =
-		'<i class="fab fa-github"></i>';
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-github").innerHTML =
+			'<i class="fab fa-github"></i> GitHub';
+	} else {
+		document.getElementById("button-github").innerHTML =
+			'<i class="fab fa-github"></i>';
+	}
 }
 
 function changeProjectsIcon() {
-	document.getElementById("button-projects").innerHTML =
-		'<i class="fas fa-terminal"></i>';
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-projects").innerHTML =
+			'<i class="fas fa-terminal"></i> Projects';
+	} else {
+		document.getElementById("button-projects").innerHTML =
+			'<i class="fas fa-terminal"></i>';
+	}
 }
 
 function changeWelcomeIcon() {
-	document.getElementById("button-welcome").innerHTML =
-		'<i class="fas fa-door-open"></i>';
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-welcome").innerHTML =
+			'<i class="fas fa-door-open"></i> Home';
+	} else {
+		document.getElementById("button-welcome").innerHTML =
+			'<i class="fas fa-door-open"></i>';
+	}
 }
 
 function changeAboutMeIcon() {
-	document.getElementById("button-about-me").innerHTML =
-		'<i class="fas fa-user"></i>';
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-about-me").innerHTML =
+			'<i class="fas fa-user"></i> About Me';
+	} else {
+		document.getElementById("button-about-me").innerHTML =
+			'<i class="fas fa-user"></i>';
+	}
+}
+
+function changeTechnoIcon() {
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-techno").innerHTML =
+			'<i class="fas fa-memory"></i> Technologies';
+	} else {
+		document.getElementById("button-techno").innerHTML =
+			'<i class="fas fa-memory"></i>';
+	}
 }
 
 function changeTiktokIcon() {
-	document.getElementById("button-tiktok").innerHTML =
-		'<i class="fab fa-tiktok"></i>';
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-tiktok").innerHTML =
+			'<i class="fab fa-tiktok"></i> TikTok';
+	} else {
+		document.getElementById("button-tiktok").innerHTML =
+			'<i class="fab fa-tiktok"></i>';
+	}
+}
+
+function changeYTIcon() {
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-youtube").innerHTML =
+			'<i class="fab fa-youtube"></i> YouTube';
+	} else {
+		document.getElementById("button-youtube").innerHTML =
+			'<i class="fab fa-youtube"></i>';
+	}
 }
 
 function changeDiscordIcon() {
-	document.getElementById("button-discord").innerHTML =
-		'<i class="fab fa-discord"></i>';
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-discord").innerHTML =
+			'<i class="fab fa-discord"></i> Discord';
+	} else {
+		document.getElementById("button-discord").innerHTML =
+			'<i class="fab fa-discord"></i>';
+	}
 }
+
+function changeOldIcon() {
+	if (window.innerWidth < textWidth) {
+		document.getElementById("old-web").innerHTML =
+			'<i class="fas fa-history"></i> Old Website';
+	} else {
+		document.getElementById("old-web").innerHTML =
+			'<i class="fas fa-history"></i>';
+	}
+}
+
+function changeContactMeIcon() {
+	if (window.innerWidth < textWidth) {
+		document.getElementById("button-contact-me").innerHTML =
+			'<i class="far fa-address-card"></i> Contact Me';
+	} else {
+		document.getElementById("button-contact-me").innerHTML =
+			'<i class="far fa-address-card"></i>';
+	}
+}
+
+changeGithubIcon();
+changeProjectsIcon();
+changeWelcomeIcon();
+changeAboutMeIcon();
+changeTiktokIcon();
+changeDiscordIcon();
+changeOldIcon();
+changeContactMeIcon();
+changeTechnoIcon();
+changeYTIcon();
